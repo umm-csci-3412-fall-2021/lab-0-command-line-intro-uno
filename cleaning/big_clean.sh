49fde0d7741c4bd3d2f7853f0a2ae7f5a8f4488f
Kyle Day <day00090@umm9020-h2tfx12.morris.umn.edu>
2021-09-02 22:27:38 -0500

    Make Fixes to big_clean.sh
    In order to satisfy the shellcheck, I added double quotations around certain variables calls. In addition, I also added an exit command as an alternative to cding into the mock directory.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -7,17 +7,17 @@ archive=$1 #This "archive" variable will serve as an argument for big_clean.sh t
 scratchDir=$(mktemp -d) #scratchDir will serve as the place for storing a "scratch" directory for storing the
                          #contents of the tar archive to be inspected.
 
-tar -x -z -f $archive --directory $scratchDir #The tar command will extract the inputted archive into the scratch directory
+tar -x -z -f "$archive" --directory "$scratchDir" #The tar command will extract the inputted archive into the scratch directory
 			         #that is created.
 
 mainDir=$(pwd) #mainDir will serve as a fallback to the original directory once the necessary steps are completed.
 
 base=$(basename "$archive" .tgz) #This variable is used to store the name of the original archive, but without the extension. This will hopefully prevent confusion or errors in reading the name.
 
-grep -r -l "DELETE ME!" $scratchDir | xargs rm #Grep will search each file in the scratch directory for "DELETE ME!"
+grep -r -l "DELETE ME!" "$scratchDir" | xargs rm #Grep will search each file in the scratch directory for "DELETE ME!"
 				       	       #After that, xargs will remove every instance of it.
 
-cd $scratchDir #Once the instances are removed, the script will return back to the main scratch directory.
+cd "$scratchDir" || exit #Once the instances are removed, the script will return back to the main scratch directory. If this is not possible, then it will exit.
 
 tar -c -z -f "cleaned_$archive" "$base" #A new tar archive will be created with the basename.
 
