532aa27dce69535acef3cc7f8279ea16d081ed61
Kyle Day <day00090@umm9020-h2tfx12.morris.umn.edu>
2021-09-02 21:45:33 -0500

    Add New Variables (mainDir, base) and Commands (grep, cd, tar, and mv)
    This commits adds:
    
    + New variables:
    + + mainDir - Serves as a fallback for when the new archive is created
    + + base - Uses basename to set the base directory; used in the tar command to set a path
    
    + New commands:
    + + grep - Used for finding instances of "DELETE ME!" and removes them with assistance from xargs
    + + cd - Used for returning back to the main scratch directory after the removal
    + + tar - Used for creating the new archive with the cleaned files
    + + mv - Used for moving the new archive to the main directory

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -4,8 +4,21 @@
 
 archive=$1 #This "archive" variable will serve as an argument for big_clean.sh to inspect.
 
-tempDir=$(mktemp -d) #tempDir will serve as the place for storing a "scratch" directory for storing the
-                                     #contents of the tar archive to be inspected.
+scratchDir=$(mktemp -d) #scratchDir will serve as the place for storing a "scratch" directory for storing the
+                         #contents of the tar archive to be inspected.
 
-tar -xzf $archive -C $tempDir #The tar command will extract the inputted archive into the temporary directory
-			      #that is created.
+tar -xzf $archive --directory $scratchDir #The tar command will extract the inputted archive into the scratch directory
+			         #that is created.
+
+mainDir=$(pwd) #mainDir will serve as a fallback to the original directory once the necessary steps are completed.
+
+base=$(basename "$archive" .tgz)
+grep -r1 "DELETE ME!" $scratchDir | xargs rm #Grep will search each file in the scratch directory for "DELETE ME!"
+					#After that, xargs will remove every instance of it, using f to
+					#ensure the deletion will happen.
+
+cd $scratchDir #Once the instances are removed, the script will return back to the main scratch directory.
+
+tar -czf "cleaned_$archive" "$base" #A new tar archive will be created with the basename.
+
+mv "cleaned_$archive" "$mainDir" #Said archive will then be moved back to the original directory.
