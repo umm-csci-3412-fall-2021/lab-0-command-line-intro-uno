9cf3587a07a40e906cad6c9997526b01e4262c62
Kyle Day <day00090@umm9020-h2tfx12.morris.umn.edu>
2021-09-02 22:17:22 -0500

    Fix grep Command, a Couple of Comments, and Readability
    This commit fixes the grep command to use the correct option "l" instead of "1". Realizing how similar the two characters are next to each other, I ended up also distinguishing all of the options with separate minus signs. In addition, I modified some comments to reflect the current state of the code. grep is now working properly. It also no longer has option f, which was added and removed during error diagnosis. The part of a comment related to it has been removed.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -7,18 +7,18 @@ archive=$1 #This "archive" variable will serve as an argument for big_clean.sh t
 scratchDir=$(mktemp -d) #scratchDir will serve as the place for storing a "scratch" directory for storing the
                          #contents of the tar archive to be inspected.
 
-tar -xzf $archive --directory $scratchDir #The tar command will extract the inputted archive into the scratch directory
+tar -x -z -f $archive --directory $scratchDir #The tar command will extract the inputted archive into the scratch directory
 			         #that is created.
 
 mainDir=$(pwd) #mainDir will serve as a fallback to the original directory once the necessary steps are completed.
 
-base=$(basename "$archive" .tgz)
-grep -r1 "DELETE ME!" $scratchDir | xargs rm #Grep will search each file in the scratch directory for "DELETE ME!"
-					#After that, xargs will remove every instance of it, using f to
-					#ensure the deletion will happen.
+base=$(basename "$archive" .tgz) #This variable is used to store the name of the original archive, but without the extension. This will hopefully prevent confusion or errors in reading the name.
+
+grep -r -l "DELETE ME!" $scratchDir | xargs rm #Grep will search each file in the scratch directory for "DELETE ME!"
+				       	       #After that, xargs will remove every instance of it.
 
 cd $scratchDir #Once the instances are removed, the script will return back to the main scratch directory.
 
-tar -czf "cleaned_$archive" "$base" #A new tar archive will be created with the basename.
+tar -c -z -f "cleaned_$archive" "$base" #A new tar archive will be created with the basename.
 
 mv "cleaned_$archive" "$mainDir" #Said archive will then be moved back to the original directory.
